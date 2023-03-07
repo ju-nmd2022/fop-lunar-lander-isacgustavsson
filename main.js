81e90875146d6cf3deb72238a732b266a8ff19d5
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2023-03-07 02:51:54 +0100

    just another commit.
    added a Startscreen, Gamescreen and a function to change between the different screens.

--- main.js
+++ main.js
@@ -3,23 +3,44 @@ let player;
 let force;
 let img;
 
+let startScreen = true;
+let gameScreen = false;
+let gameOverScreen = false;
+
 function setup() {
   createCanvas(800, 600);
 
+  img = loadImage("images/60parsecs.png");
+
   racetrack = new Racetrack();
   player = new Player();
+}
+
+function screenChanger() {
+  if (startScreen === true) {
+    image(img, 0, 0);
+  }
+
+  if (keyCode === 13) {
+    startScreen = false;
+    gameScreen = true;
+  }
 }
 
 function draw() {
   clear();
   background("black");
 
-  racetrack.animate();
-  racetrack.update();
-  racetrack.boost();
+  if (gameScreen === true) {
+    racetrack.animate();
+    racetrack.update();
+    racetrack.boost();
 
-  player.animate();
-  player.update();
-  player.move();
-  player.boost();
+    player.animate();
+    player.update();
+    player.move();
+    player.boost();
+  }
+
+  screenChanger();
 }
